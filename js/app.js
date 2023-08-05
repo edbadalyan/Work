c4a9879b5b74fb6b630ad8aa01e683aa33053e3d
edbadalyan <126268442+edbadalyan@users.noreply.github.com>
2023-08-05 16:24:45 +0400

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menuMobile = document.querySelector(".menu-mobile");
-
-menuBtn.addEventListener('click', ()=>{
-    menuMobile.classList.toggle('menu--open');
-});
